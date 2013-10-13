13e1f83976be6a7c736ada048b66c70591a238d3
Rui Peres <sauron.is.alive@gmail.com>
2013-10-13 21:14:33 +0100

    Added StartingViewController
    - Added as well a category to be used to setup the new VC's UI.
    - Added a Test class for this specific VC

---
+++ NSCoders/ViewControllers/Starting ViewController/Category/NCStartingViewController+UI.h
@@ -0,0 +1,18 @@
+//
+//  NCStartingViewController+UI.h
+//  NSCoders
+//
+//  Created by Rui Peres on 13/10/2013.
+//  Copyright (c) 2013 NSCoders. All rights reserved.
+//
+
+#import "NCStartingViewController.h"
+
+/**
+ Category used to setup the UI for a StartingViewController
+ */
+@interface NCStartingViewController (UI)
+
+- (void)setupUI;
+
+@end
